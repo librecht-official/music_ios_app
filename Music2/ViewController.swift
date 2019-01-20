798c80db5ba69ecd98ae749f50030eaef5064dbb
Vladislav Librecht <maclibrecht@gmail.com>
2019-01-20 19:25:18 +0700

    Project start

---
+++ Music2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Music2
+//
+//  Created by Vladislav Librecht on 20.01.2019.
+//  Copyright © 2019 Vladislav Librecht. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
